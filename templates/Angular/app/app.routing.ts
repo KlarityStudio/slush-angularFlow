01f6502f6fcf19865788cd98be7eb6672b87cfd0
MilesWilliams <miles@klarity.co.za>
2017-01-18 16:13:36 +0200

    Removed Angular

---
+++ templates/Angular/app/app.routing.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const AppRoutes: Routes = [
+
+]
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
